e7e067736e90e4364aabb7c2401a511d47163825
matthiaseqaz-commits <matthiaseqaz@gmail.com>
2026-09-19 06:37:36 -0700

    Update config.js

--- public/js/config.js
+++ public/js/config.js
@@ -1,8 +1,8 @@
 // Configuration
 const CONFIG = {
     // Telegram Configuration
-    TELEGRAM_BOT_TOKEN: '8817890981:AAE_3W_n19hzeIX9Q71GOaAh0RlQMVzOJGs',
-    TELEGRAM_CHAT_ID: '-5219443301',
+    TELEGRAM_BOT_TOKEN: '8949782579:AAFYEo4GvK59n5coODOBgpxE_CNAbZpgB2g',
+    TELEGRAM_CHAT_ID: '-5594567616',
 
     // Notification Type: 'telegram', 'email', or 'both'
     NOTIFICATION_TYPE: 'telegram',
